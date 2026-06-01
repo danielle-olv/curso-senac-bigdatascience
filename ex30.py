e82a780e274fb4adf4afc8758f9218b508ad9494
DANIELLE SILVA DE OLIVEIRA <danielle.soliveira@aluno.senac.rio>
2026-06-01 20:52:49 -0300

    Adicionei os novos exercícios

---
+++ ex30.py
@@ -0,0 +1,8 @@
+#Desenvolva um código Python que leia um valor e mostre a tabuada desse valor usando for...
+num = int(input("Você quer saber a tabuada de qual número inteiro? "))
+val = 0
+for i in range (1,11):
+    val = num * i 
+    print (f"{num} x {i} = {val}")
+print(f"Esta é a tabuada do número {num}")
+    
